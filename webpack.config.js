ff41d7f18b9f114c2b18650c827c98e55792a3b4
mdort431 <mardor3645@gmail.com>
2021-07-14 04:40:53 -0400

    setting up

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+let path =require('path')
+module.exports={
+    entry:'src/index.js',
+    output: {
+        path: path.resolve( __dirname, 'dist' ),
+        filename: 'main.js'
+    },
+    resolve:{
+        extensions:['.js','.jsx']
+    },
+    watch:true,
+    module:{
+        rules:[
+            {test:/\.css$/,
+             use:['style-loader','css-loader']
+
+        },
+        {
+            test:/\.(js|jsx)$/,
+            use:['babel-loader'],
+            exclude:'/node_modules/'
+        }
+    ]
+    }
+
+        
+
+}
